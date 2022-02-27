339a25881e6346a793047ade3a65564db29365df
anjal-i <anjaliahuja99@yahoo.com>
2022-02-27 22:26:10 +1100

    Update MovieObject to highlight cheapest price

--- src/Components/MovieObject.js
+++ src/Components/MovieObject.js
@@ -4,7 +4,7 @@ import "./MovieObject.css";
 // renders a movie object, w/ title and both prices
 function MovieObject({ title, cwPrice, fwPrice }) {
   // find fw price by finding matching title
-
+  const cwIsCheaper = cwPrice < fwPrice;
   return (
     <div className="movie-container">
       <div className="movie-image"></div>
@@ -15,7 +15,9 @@ function MovieObject({ title, cwPrice, fwPrice }) {
             <p>Cinema World</p>
           </div>
           <div className="col">
-            <p>$ {cwPrice.toFixed(2)}</p>
+            <p className={cwIsCheaper ? "highlighted" : ""}>
+              $ {cwPrice.toFixed(2)}
+            </p>
           </div>
         </div>
         <div className="row">
@@ -23,7 +25,9 @@ function MovieObject({ title, cwPrice, fwPrice }) {
             <p>Film World</p>
           </div>
           <div className="col">
-            <p>$ {fwPrice.toFixed(2)} </p>
+            <p className={cwIsCheaper ? "" : "highlighted"}>
+              $ {fwPrice.toFixed(2)}{" "}
+            </p>
           </div>
         </div>
       </div>
